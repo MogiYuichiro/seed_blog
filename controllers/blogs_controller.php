b878221cb1d658fe1fb7bb4224d10ca207e08985
茂木雄一郎 <mogi.3614@gmail.com>
2016-02-16 14:53:20 +0800

    作業途中

--- controllers/blogs_controller.php
+++ controllers/blogs_controller.php
@@ -5,7 +5,7 @@ require('models/blog.php');
 //コントローラのクラスをインスタンス化
 $controller = new BlogsController();
 
-//アクション名によって、呼び出すメッソドを変える
+//アクション名によって、呼び出すメソッドを変える
 //$action(グローバル変数)は、routes.phpで定義されているもの
 switch ($action) {
 	case 'index':
@@ -21,13 +21,25 @@ switch ($action) {
 
 
  class BlogsController{
- 	public function index(){
+ 		//プロパィ
+ 		private $action='';
+ 		private $resource='';
+
+ 		public function index(){
+ 			//モデルを呼び出す
+ 			// $blog = new Blog();
+ 			// $blog->index();
+
+ 		
  		///モデルを呼び出す
  		$blog = new Blog();
  		$blog->index();
 
+ 		$this->action ='index';
+
  		//ビューを呼び出す
- 		require('views/blogs/index.php');
+ 		require('views/layout/application.php');
+ 		//require('views/blogs/index.php');
  	}
  }
  ?>
